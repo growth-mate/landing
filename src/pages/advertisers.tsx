a9d33e0cc54365d22d52bc5d61cb3b50b749c9e2
ivan.dimitrov <ivan.dimitrov@fit.fraunhofer.de>
2024-05-10 14:28:46 +0200

    chaged text

--- src/pages/advertisers.tsx
+++ src/pages/advertisers.tsx
@@ -13,7 +13,7 @@ export default function Advertisers() {
       <div className="container">
         <div>
           <h1>Advertisers</h1>
-          <p>By understanding users' investments and interests through blockchain analysis, advertisers can deliver precisely tailored advertisements, significantly increasing the likelihood of engagement and conversion. This strategy can lead to a substantial increase in return on investment and optimize ad spend effectiveness. </p>
+          <p>By understanding users' investments and interests through blockchain analysis, advertisers can <br/> deliver precisely tailored advertisements, significantly increasing the likelihood of engagement <br/> and conversion. This strategy can lead to a substantial increase in return on investment and <br/> optimize ad spend effectiveness. </p>
         </div>
       </div>
       <style jsx>{`
