410a66814e859787f3ffe39ca71c37e239a04364
mohamedalichelbi <mohamedalichelbi123@gmail.com>
2024-06-28 17:31:21 +0200

    Feat: add Amplitude analytics

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,6 +13,9 @@ export default function Layout({ children }: Props) {
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="apple-touch-icon" href="/wlogo.png" />
         <meta name="theme-color" content="#fff" />
+        <script src="https://cdn.amplitude.com/libs/analytics-browser-2.7.4-min.js.gz"></script>
+        <script src="https://cdn.amplitude.com/libs/plugin-session-replay-browser-1.4.1-min.js.gz"></script>
+        <script src="https://cdn.amplitude.com/libs/plugin-autocapture-browser-0.9.0-min.js.gz"></script>
       </Head>
       <nav>
         <Navigation />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,18 @@ import { AppProps } from "next/app";
 // NOTE: Do not move the styles dir to the src.
 // They are used by the Netlify CMS preview feature.
 import "../../public/styles/global.css";
+import React from "react";
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Enable Amplitude
+  React.useEffect(() => {
+    // Amplitude tracking
+    if (window.hasOwnProperty("amplitude")) {
+      // @ts-ignore
+      window.amplitude.add(window.sessionReplay.plugin({sampleRate: 1})).promise.then(function() {window.amplitude.add(window.amplitudeAutocapturePlugin.plugin());window.amplitude.init('0f2cfd7a4e85d321451f8f8c3729e74');});
+    }
+  }, []);  
+
   return <Component {...pageProps} />;
 }
